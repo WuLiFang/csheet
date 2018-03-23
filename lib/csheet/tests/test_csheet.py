93982672cd7ae90bc61279a6408719e42efe4291
NateScarlet <NateScarlet@Gmail.com>
2018-03-23 21:34:36 +0800

    Modify .env

--- lib/csheet/tests/test_csheet.py
+++ lib/csheet/tests/test_csheet.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 import pickle
 import sys
+print(sys.path)
 from tempfile import mktemp
 from unittest import TestCase, main, skip
 
