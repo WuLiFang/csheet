edf21c4a32e60ff5dfb85d82ae7d90f2fa370c39
NateScarlet <NateScarlet@Gmail.com>
2018-04-27 00:39:36 +0800

    Use notify js(WIP)

--- src/socketio.ts
+++ src/socketio.ts
@@ -1,9 +1,11 @@
 import * as io from 'socket.io-client';
 import * as $ from 'jquery';
+import Notify from 'notifyjs';
 
 
 $(document).ready(
     () => {
+        Notify.requestPermission()
         const socket = io(`/`, { path: '/api/socket.io' })
         socket.on('new asset',
             (message: string) => {
@@ -11,11 +13,11 @@ $(document).ready(
             }
         )
         socket.on('connect',
-            () => { console.log('connected') }
+            () => { new Notify('已建立连接', { timeout: 2 }).show() }
         )
         socket.on('disconnect',
             () => {
-                console.log('disconnect')
+                new Notify('连接断开', { timeout: 2 }).show()
             }
         )
     }
