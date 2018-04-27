507849a77c232f76024713e34759174f1d1c382c
NateScarlet <NateScarlet@Gmail.com>
2018-04-27 18:17:05 +0800

    Improve crt generation

--- generate_crt.ps1
+++ generate_crt.ps1
@@ -1,5 +1,2 @@
-openssl req -x509 -newkey rsa:4096 -days 365 -keyout key.pem -out csheet.crt
-openssl rsa -in key.pem -out csheet.key 
-if ($?) {
-    Remove-Item key.pem 
-}
+# See https://stackoverflow.com/questions/43665243/invalid-self-signed-ssl-cert-subject-alternative-name-missing for req.conf
+openssl req -x509 -nodes -newkey rsa:4096 -days 365 -utf8 -sha256 -config req.conf -keyout csheet.key -out csheet.crt
