40d6b48f0c84dca759b0b435dcfe3d5a170afbd1
NateScarlet <NateScarlet@Gmail.com>
2018-04-28 18:49:29 +0800

    Use docker-compose to build

--- update_image.ps1
+++ update_image.ps1
@@ -1,6 +1,19 @@
+
+param (
+    [switch]$build = $false
+)
+
 $NUM_CORES = Get-WmiObject -Class Win32_ComputerSystem | Select-Object -ExpandProperty "NumberOfLogicalProcessors"
 $env:SENTRY_DSN = Get-Content .\SENTRY_DSN
 $env:NUM_WORKERS = (($NUM_CORES * 2) + 1)
 
 docker-machine env | Invoke-Expression
-docker-compose up -d
+if ($build) {
+    git clean -fdX *.pyc
+    npx webpack --mode production
+    docker-compose up -d --build
+    docker system prune -f
+}
+else {
+    docker-compose up -d
+}
