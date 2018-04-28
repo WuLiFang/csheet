d4cc947ebd855df0eb917d94eb4971601ad72529
NateScarlet <NateScarlet@Gmail.com>
2018-04-28 14:09:24 +0800

    Not generate crt, use windows server AD CA
    See https://docs.microsoft.com/en-us/previous-versions/windows/it-pro/windows-server-2008-R2-and-2008/ff625722(v=ws.10)

--- generate_crt.ps1
+++
@@ -1,2 +0,0 @@
-# See https://stackoverflow.com/questions/43665243/invalid-self-signed-ssl-cert-subject-alternative-name-missing for req.conf
-openssl req -x509 -nodes -newkey rsa:4096 -days 365 -utf8 -sha256 -config req.conf -keyout csheet.key -out csheet.crt
