fe3cd171ff202dfad0f865f161f3dce17df39a8d
NateScarlet <NateScarlet@Gmail.com>
2018-07-23 18:52:58 +0800

    Improve submit file parse

--- lib/csheet/page/cgteamwork.py
+++ lib/csheet/page/cgteamwork.py
@@ -187,8 +187,10 @@ def _get_submit_file(submit_file_data):
     if not submit_file_data:
         return
     try:
-        return json.loads(submit_file_data)['file_path'][0]
-    except (TypeError, KeyError, IndexError):
+        data = json.loads(submit_file_data)
+        filelist = data.get('file_path') or data.get('path')
+        return filelist[0]
+    except (TypeError, IndexError):
         LOGGER.warn('Parse submit file data fail: %s', repr(submit_file_data))
     return None
 
