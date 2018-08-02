21006284ff2b559db2ec00d2b3e190a13d603d99
NateScarlet <NateScarlet@Gmail.com>
2018-08-02 18:38:19 +0800

    Set task autoretry

--- lib/csheet/task.py
+++ lib/csheet/task.py
@@ -6,13 +6,17 @@ from __future__ import (absolute_import, division, print_function,
 
 import logging
 
+import sqlalchemy.exc
+
 from . import database
 from .core import CELERY
 
 LOGGER = logging.getLogger(__name__)
 
 
-@CELERY.task(ignore_result=True)
+@CELERY.task(ignore_result=True,
+             autoretry_for=(sqlalchemy.exc.OperationalError,),
+             retry_backoff=True)
 def update_page(page):
     """Update page data.  """
 
