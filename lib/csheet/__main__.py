7de5383d38f9df9097bfdda89011ed2f760ffc22
NateScarlet <NateScarlet@Gmail.com>
2018-08-30 11:02:29 +0800

    Refactor cli with `fire`

--- lib/csheet/__main__.py
+++ lib/csheet/__main__.py
@@ -1,17 +1,16 @@
 #! /usr/bin/env python2
 # -*- coding=UTF-8 -*-
-"""GUI for csheet creation.  """
+"""csheet(colorsheet/contactsheet) CLI.  """
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
-import argparse
 import logging
 import os
 import webbrowser
-from socket import gethostbyname, gethostname
+
+import fire
 
 from wlf import mp_logging
-from wlf.path import Path
 from wlf.path import get_encoded as e
 
 from . import database, filetools, page
@@ -21,58 +20,45 @@ from .core import APP, CELERY, SOCKETIO
 LOGGER = logging.getLogger(__name__)
 
 
-def run_server(port=5000, local_dir=None):
-    """Run csheet server at @port.  """
+def _setup_logging(default_level=logging.INFO):
+    try:
+        mp_logging.basic_config(
+            level=logging.getLevelName(os.getenv('LOGLEVEL')))
+    except ValueError:
+        mp_logging.basic_config(level=default_level)
 
-    APP.config['local_dir'] = local_dir
 
-    host_ip = gethostbyname(gethostname())
-    address = 'https://{}:{}'.format(host_ip, port)
+def serve(host='0.0.0.0', port=80, storage='/srv/csheet'):
+    """Run csheet server forever.
+        port (int, optional): Defaults to 80. Listenling port.
+        storage (str, optional): Defaults to '/srv/csheet'. Storage path.
+    """
+
+    _setup_logging()
+    APP.config['STORAGE'] = storage
+    APP.config['ENGINE_URL'] = 'sqlite:///{}\\csheet.db'.format(storage)
+    CELERY.conf['task_always_eager'] = True
+    APP.config['IS_STANDALONE'] = True
+
+    address = 'https://{}:{}'.format(host, port)
     print(address)
     LOGGER.info('服务器运行于: %s', address)
-    SOCKETIO.run(APP, '0.0.0.0', port, debug=False)
 
-    return (host_ip, port)
-
-
-def main():
-
-    desc = '吾立方色板工具 {}'.format(__version__)
-    parser = argparse.ArgumentParser(description=desc)
-    parser.add_argument('-d', '--dir', metavar='目录', required=False,
-                        help='包含色板所需图像的目录')
-    parser.add_argument('-p', '--port', metavar='端口', type=int, required=False,
-                        help='服务器运行端口')
-    parser.add_argument('-s', '--storage', metavar='文件仓库路径',
-                        type=lambda x: unicode(Path(x)), required=False,
-                        help='生成的文件集中存放至此')
-    args = parser.parse_args()
-
-    try:
-        mp_logging.basic_config(level=os.getenv('LOGLEVEL', logging.INFO))
-    except ValueError:
-        mp_logging.basic_config(level=logging.INFO)
-
-    if args.port:
-        if args.storage:
-            APP.config['storage'] = args.storage
-        CELERY.conf['task_always_eager'] = True
-        APP.config['IS_STANDALONE'] = True
-        run_server(args.port, args.dir)
-    elif args.dir:
-        render_staic_page(args.dir)
+    SOCKETIO.run(APP, host, port, debug=False)
 
 
-def render_staic_page(dir_):
+def _render(root):
+    """Render page for a folder """
+
     # TODO: Implement static page.
-    page_ = page.LocalPage(dir_)
+    page_ = page.LocalPage(root)
     page_.static_folder = filetools.path('static')
     with database.session_scope() as sess:
         page_.update(sess)
         html = page_.render(page_.videos(sess))
 
     target = os.path.join(os.path.abspath(
-        os.path.dirname(dir_)), '{}.html'.format(page_.title))
+        os.path.dirname(root)), '{}.html'.format(page_.title))
     with open(e(target), 'w') as f:
         f.write(html.encode('utf-8'))
     LOGGER.info('生成色板: %s', target)
@@ -81,4 +67,4 @@ def render_staic_page(dir_):
 
 
 if __name__ == '__main__':
-    main()
+    fire.Fire(name='csheet')
