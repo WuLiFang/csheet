ee5855dbd13c581193f773a6f58982200a588465
NateScarlet <NateScarlet@Gmail.com>
2018-08-30 16:32:01 +0800

    Add validator to form

--- src/components/CGTeamWorkPageForm.vue
+++ src/components/CGTeamWorkPageForm.vue
@@ -1,15 +1,18 @@
 <template lang="pug">
   ElForm(
-    label-width='80px' 
+    label-width='80px'
+    :model='form'
+    :rules='rules'
+    ref='form'
   )
-    ElFormItem(label='项目')
+    ElFormItem(label='项目' prop='project')
       ElSelect(v-model='form.project' @change='setDefaultPrefix')
         ElOption(v-for='i in projects' :key='i.code', :value='i.name')
-    ElFormItem(label='流程')
+    ElFormItem(label='流程' prop='pipeline')
       ElRadio(v-model='form.pipeline' label='合成')
       ElRadio(v-model='form.pipeline' label='灯光')
       ElRadio(v-model='form.pipeline' label='动画')
-    ElFormItem(label='前缀')
+    ElFormItem(label='前缀' prop='prefix')
       ElInput(v-model='form.prefix' ref='inputPrefix')
     ElFormItem
       ElButton(type='primary' @click='open') 打开
@@ -48,6 +51,29 @@ export default Vue.extend({
         pipeline: getCookie('pipeline'),
         prefix: getCookie('prefix'),
       },
+      rules: {
+        project: [{ required: true, message: '请选择项目', trigger: 'blur' }],
+        pipeline: [
+          { required: true, message: '请选择流程', trigger: 'change' },
+        ],
+        prefix: [
+          { required: true, message: '请输入前缀', trigger: 'blur' },
+          {
+            validator: (
+              rule: any,
+              value: string,
+              callback: (error?: Error) => void,
+            ) => {
+              if (!value.includes('_')) {
+                callback(new Error('至少包含一个下划线`_`'));
+              } else {
+                callback();
+              }
+            },
+            trigger: 'change',
+          },
+        ],
+      },
       projects,
       is_opening: false,
     };
@@ -57,15 +83,27 @@ export default Vue.extend({
       return (this.$refs.inputPrefix as ElInput).$refs
         .input as HTMLInputElement;
     },
+    formComponent(): ElForm {
+      return this.$refs.form as ElForm;
+    },
   },
   methods: {
     open() {
-      showFullScreenLoading();
-      location.href = buildURL('', this.form);
+      this.formComponent.validate((valid: boolean) => {
+        if (valid) {
+          showFullScreenLoading();
+          location.href = buildURL('', this.form);
+        }
+      });
     },
     pack() {
-      showFullScreenLoading();
-      location.href = buildURL('', { ...this.form, pack: '1' });
+      this.formComponent.validate((valid: boolean) => {
+        if (valid) {
+          showFullScreenLoading();
+          location.href = buildURL('', this.form);
+          location.href = buildURL('', { ...this.form, pack: '1' });
+        }
+      });
     },
     setDefaultPrefix() {
       const project = this.projects.find(i => i.name === this.form.project);
--- src/components/LocalPageForm.vue
+++ src/components/LocalPageForm.vue
@@ -1,8 +1,11 @@
 <template lang="pug">
   ElForm(
-    label-width='80px' 
+    label-width='80px'
+    :model='form'
+    :rules='rules'
+    ref='form'
   )
-    ElFormItem(label='路径')
+    ElFormItem(label='路径' prop='root')
       ElInput(v-model='form.root')
     ElFormItem
       ElButton(type='primary' @click='open') 打开
@@ -37,16 +40,49 @@ export default Vue.extend({
       form: {
         root: getCookie('root', 'Y:\\资源\\动态素材\\灰、尘、土'),
       },
+      rules: {
+        root: [
+          { required: true, message: '请填写文件夹路径', trigger: 'blur' },
+          {
+            validator: (
+              rule: any,
+              value: string,
+              callback: (error?: Error) => void,
+            ) => {
+              if (!(/^\/.*/.test(value) || /^\w:[\\/].*/.test(value))) {
+                callback(new Error('请使用绝对路径'));
+              } else if (/^\/$/.test(value) || /^\w:[\\/]$/.test(value)) {
+                callback(new Error('不允许使用根目录'));
+              } else {
+                callback();
+              }
+            },
+          },
+        ],
+      },
     };
+  },
+  computed: {
+    formComponent(): ElForm {
+      return this.$refs.form as ElForm;
+    },
   },
   methods: {
     open() {
-      showFullScreenLoading();
-      location.href = buildURL('local', this.form);
+      this.formComponent.validate((valid: boolean) => {
+        if (valid) {
+          showFullScreenLoading();
+          location.href = buildURL('local', this.form);
+        }
+      });
     },
     pack() {
-      showFullScreenLoading();
-      location.href = buildURL('local', { ...this.form, pack: '1' });
+      this.formComponent.validate((valid: boolean) => {
+        if (valid) {
+          showFullScreenLoading();
+          location.href = buildURL('local', { ...this.form, pack: '1' });
+        }
+      });
     },
   },
 });
