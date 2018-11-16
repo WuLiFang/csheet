3dab91521e54d885fc6bf09e4de2332d46a75e1e
NateScarlet <NateScarlet@Gmail.com>
2018-11-16 15:12:06 +0800

    Correct shebang

--- lib/csheet/__main__.py
+++ lib/csheet/__main__.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding=UTF-8 -*-
 """csheet(colorsheet/contactsheet) CLI.  """
 from __future__ import (absolute_import, division, print_function,
