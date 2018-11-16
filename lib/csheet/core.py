fc424c8131374c665f46a350f20171a8f52d0eea
NateScarlet <NateScarlet@Gmail.com>
2018-11-16 13:59:12 +0800

    Read secret key from env

--- lib/csheet/core.py
+++ lib/csheet/core.py
@@ -6,6 +6,7 @@ from __future__ import (absolute_import, division, print_function,
 
 import logging
 import logging.config
+import os
 
 from celery import Celery
 from flask import Flask
@@ -17,8 +18,8 @@ from .__about__ import __name__ as name
 from .__about__ import __version__
 
 APP = Flask(name, root_path=filetools.dist_path())
-APP.secret_key = ('}w\xb7\xa3]\xfaI\x94Z\x14\xa9\xa5}\x16\xb3'
-                  '\xf7\xd6\xb2R\xb0\xf5\xc6*.\xb3I\xb7\x066V\xd6\x8d')
+APP.secret_key = os.getenv('SECRET_KEY', ('}w\xb7\xa3]\xfaI\x94Z\x14\xa9\xa5}\x16\xb3'
+                                          '\xf7\xd6\xb2R\xb0\xf5\xc6*.\xb3I\xb7\x066V\xd6\x8d'))
 APP.config.from_object('csheet.default_settings')
 APP.config.from_envvar('CSHEET_SETTINGS', silent=True)
 
