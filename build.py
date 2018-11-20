7e642d27126e58f76f672d235c3532f9d0f711d3
NateScarlet <NateScarlet@Gmail.com>
2018-11-20 20:08:45 +0800

    Make build script independ on `csheet` module

--- build.py
+++ build.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 """Build script for docker."""
 
+import json
 import subprocess
 
 import fire
 
-from csheet.__about__ import __version__
 from wlf.pathtools import make_path_finder
 
 file_path = make_path_finder(__file__)  # pylint: disable=invalid-name
+with open(file_path('package.json')) as f:
+    __version__ = json.load(f)['version']
 
 
 def build_image(name='csheet'):
