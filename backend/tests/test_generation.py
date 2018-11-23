2db74ab464f7c8d3463aad932d4d7bdf617b185f
NateScarlet <NateScarlet@Gmail.com>
2018-11-23 11:52:32 +0800

    Update tests

--- backend/tests/test_generation.py
+++ backend/tests/test_generation.py
@@ -3,11 +3,9 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
-import util
 from csheet import generation
 
 
-def test_execute_generate_task():
-    util.setup()
-    for i in generation.GENERATION_TASKS:
-        generation.execute_generate_task(**i)
+def test_discover_generate_task():
+    generation.discover_light_tasks()
+    generation.discover_heavy_tasks()
