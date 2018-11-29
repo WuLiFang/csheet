be787e223764983134fbb1c3ab16a7f0c55bad0c
NateScarlet <NateScarlet@Gmail.com>
2018-11-29 13:01:13 +0800

    Accept extra arguments in docker entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,12 +14,12 @@ if [ "$1" = "run" ]; then
             -b 0.0.0.0:80 csheet:APP
         ;;
     "beat" )
-        celery -A csheet:CELERY beat
+        celery -A csheet:CELERY beat ${@:3}
         ;;
     "worker" )
         useradd worker
         chown worker ${STORAGE:-/srv/csheet}
-        celery -A csheet:CELERY worker --uid $(id -u worker) --gid $(id -g worker)
+        celery -A csheet:CELERY worker --uid $(id -u worker) --gid $(id -g worker) ${@:3}
         ;;
     * )
         echo "Can not recognize argument: $2"
