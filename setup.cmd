9535d22f83a170446cc3a6be747d768cb6cfe19f
NateScarlet <NateScarlet@Gmail.com>
2019-02-20 17:25:53 +0800

    Improve `setup.cmd`

--- setup.cmd
+++ setup.cmd
@@ -1,6 +1,7 @@
 cd /d %~dp0
 py -3 -m pip install -U pip pipenv
 py -3 -m pipenv install -d
-cmd /c npm --registry=https://registry.npm.taobao.org i -d
+npm i -g mirror-config-china --registry=https://registry.npm.taobao.org
+npm i -d
 npm run build
 PAUSE
