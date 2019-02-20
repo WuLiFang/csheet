fe8dbfae5caf81a5db84897bde4e35f4bc9f6560
NateScarlet <NateScarlet@Gmail.com>
2019-02-20 18:31:42 +0800

    Improve `setup.cmd`

--- setup.cmd
+++ setup.cmd
@@ -1,7 +1,10 @@
 cd /d %~dp0
+ping -n 1 google.com ^
+    || set "PIP_MIRROR=https://mirrors.aliyun.com/pypi/simple" ^
+    && set "PIPENV_PYPI_MIRROR=https://mirrors.aliyun.com/pypi/simple" ^
+    && cmd /c npm i -g mirror-config-china --registry=https://registry.npm.taobao.org
 py -3 -m pip install -U pip pipenv
 py -3 -m pipenv install -d
-npm i -g mirror-config-china --registry=https://registry.npm.taobao.org
-npm i -d
-npm run build
+cmd /c npm i -d
+cmd /c npm run build
 PAUSE
