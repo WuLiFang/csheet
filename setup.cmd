5b679f2ad8b4e3386825f3f85a52532c87b7440c
NateScarlet <NateScarlet@Gmail.com>
2019-05-13 23:02:21 +0800

    chore: replace pipenv with pip+virtualenv

--- setup.cmd
+++ setup.cmd
@@ -8,21 +8,23 @@ py -3.7 --version || CALL :SUDO cinst -y python3 && @ setx "PIPENV_VENV_IN_PROJE
 cmd /c npm --version || CALL :SUDO cinst -y nodejs-lts && @ cmd /c %ProgramData%/chocolatey/bin/RefreshEnv.cmd
 
 REM Setup mirrors
-set "PIPENV_VENV_IN_PROJECT=1" 
 ping -n 1 google.com ^
     || set "PIP_MIRROR=https://mirrors.aliyun.com/pypi/simple" ^
-    && set "PIPENV_PYPI_MIRROR=https://mirrors.aliyun.com/pypi/simple" ^
     && cmd /c npm i -g mirror-config-china --registry=https://registry.npm.taobao.org
 
 REM Install dependencies
-py -3 -m pip install -U pip pipenv
-py -3 -m pipenv install -d
-cmd /c npm i -d
+py -3.7 -m pip install -U pip virtualenv
+IF NOT EXIST .venv/Scripts/activate.bat (py -3.7 -m virtualenv .venv)
+call .venv/Scripts/activate.bat
+pip install -r requirements.txt -r requirements-dev.txt
+cmd /c npm i
 
 REM Build frontend files
 cmd /c npm run build
+
 PAUSE
+GOTO :EOF
 
 :SUDO
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "Start-Process -Wait -FilePath powershell.exe -ArgumentList @('-NoProfile', '-InputFormat', 'None', '-ExecutionPolicy', 'Bypass', '-Command', 'cd', '%cd%', ';', """%*""") -verb RunAs" 
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "Start-Process -Wait -FilePath powershell.exe -ArgumentList @('-NoProfile', '-InputFormat', 'None', '-ExecutionPolicy', 'Bypass', '-Command', 'cd', '%cd%', ';', """%*""") -verb RunAs"
 GOTO :EOF
