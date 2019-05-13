0b78fe676597cb94dfc313f43b131ae25b84acf4
NateScarlet <NateScarlet@Gmail.com>
2019-05-14 01:47:44 +0800

    chore: fix start.cmd

--- start.cmd
+++ start.cmd
@@ -1,11 +1,15 @@
 TITLE csheet server
 cd /d %~dp0
 IF NOT EXIST dist cmd /c setup.cmd
-set "PIPENV_VENV_IN_PROJECT=1"
 ffmpeg -version || CALL :SUDO cinst -y ffmpeg && cmd /c %ProgramData%/chocolatey/bin/RefreshEnv.cmd
-py -3 -m pipenv run python -m csheet runserver --host 0.0.0.0 --port 80
+CALL .venv/Scripts/activate.bat
+SET "PYTHONPATH=backend"
+SET "FLASK_APP=csheet:APP"
+python -m csheet runserver --host 0.0.0.0 --port 80
+
 PAUSE
+GOTO :EOF
 
 :SUDO
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "Start-Process -Wait -FilePath powershell.exe -ArgumentList @('-NoProfile', '-InputFormat', 'None', '-ExecutionPolicy', 'Bypass', '-Command', 'cd', '%cd%', ';', """%*""") -verb RunAs" 
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "Start-Process -Wait -FilePath powershell.exe -ArgumentList @('-NoProfile', '-InputFormat', 'None', '-ExecutionPolicy', 'Bypass', '-Command', 'cd', '%cd%', ';', """%*""") -verb RunAs"
 GOTO :EOF
