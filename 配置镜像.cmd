ac0eb88062a97899698d1a40d4e885b2565ccc7e
NateScarlet <NateScarlet@Gmail.com>
2020-01-11 15:58:06 +0800

    ci: correct ci script

--- 配置镜像.cmd
+++ 配置镜像.cmd
@@ -1,3 +1,3 @@
 ping -n 1 google.com ^
     || pip config set global.index-url https://mirrors.aliyun.com/pypi/simple ^
-    && cmd /c npm i -g mirror-config-china --registry=https://registry.npm.taobao.org
+    && cmd /c npx mirror-config-china --registry=https://registry.npm.taobao.org
