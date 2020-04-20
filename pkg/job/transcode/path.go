44927f54db57428a2de4052a481b915145e48dfb
NateScarlet <NateScarlet@Gmail.com>
2020-04-20 12:16:31 +0800

    style: remove unused code

--- pkg/job/transcode/path.go
+++ pkg/job/transcode/path.go
@@ -4,11 +4,8 @@ import (
 	"os"
 	"path"
 	"path/filepath"
-	"time"
 
-	"github.com/WuLiFang/csheet/pkg/db"
 	"github.com/WuLiFang/csheet/pkg/filestore"
-	"github.com/WuLiFang/csheet/pkg/model/file"
 )
 
 func replaceExt(v string, ext string) string {
@@ -17,31 +14,6 @@ func replaceExt(v string, ext string) string {
 		return v
 	}
 	return v[:len(v)-len(e)] + ext
-}
-
-func isExists(v string) bool {
-	_, err := file.FindByPath(v)
-	if err == nil {
-		return true
-	}
-	if err == db.ErrKeyNotFound {
-		return false
-	}
-	if err != nil {
-		logger.Errorw("db error", "error", err)
-	}
-	return true
-}
-
-func modTime(v string) time.Time {
-	f, err := file.FindByPath(v)
-	if err == db.ErrKeyNotFound {
-		err = nil
-	}
-	if err != nil {
-		logger.Errorw("db error", "error", err)
-	}
-	return f.ModTime
 }
 
 func removeStoreFile(v string) (err error) {
