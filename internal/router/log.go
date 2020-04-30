368f12bfdfefda3f1c0fe8d8557d40049a78757f
NateScarlet <NateScarlet@Gmail.com>
2020-04-30 21:00:22 +0800

    chore: add logs to internal/router

---
+++ internal/router/log.go
@@ -0,0 +1,5 @@
+package router
+
+import "github.com/WuLiFang/csheet/v6/pkg/logging"
+
+var logger = logging.GetLogger("router")
--- internal/router/router.go
+++ internal/router/router.go
@@ -12,6 +12,7 @@ import (
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/WuLiFang/csheet/v6/internal/config"
 	"github.com/WuLiFang/csheet/v6/pkg/api"
+	"github.com/WuLiFang/csheet/v6/pkg/db"
 	"github.com/WuLiFang/csheet/v6/pkg/filestore"
 	"github.com/WuLiFang/csheet/v6/pkg/model/file"
 	"github.com/WuLiFang/csheet/v6/pkg/model/presentation"
@@ -64,12 +65,18 @@ func New() *gin.Engine {
 			case http.StatusNotModified:
 				filestore.SetAccessTime(path.Join(filestore.Dir, filepath), time.Now())
 			case http.StatusNotFound:
+				logger.Infow("not found requested file", "filepath", filepath, "clientIP", c.ClientIP())
 				f, err := file.FindByPath(filepath)
 				if err == nil {
 					f.Delete()
 				}
+				if err != db.ErrKeyNotFound {
+					logger.Errorw("find file failed", "error", err)
+				}
+
 				ps, err := presentation.FindByPath(filepath)
 				if err == nil {
+					logger.Debugw("try generate missing file", "presentations", ps)
 					for _, p := range ps {
 						if p.Thumb == filepath {
 							p.Thumb = ""
@@ -81,6 +88,8 @@ func New() *gin.Engine {
 						}
 						p.Save()
 					}
+				} else {
+					logger.Errorw("find presentation failed", "error", err)
 				}
 			}
 		}(status, c.Param("filepath"))
