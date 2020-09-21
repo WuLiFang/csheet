fa9ad347fa981059f28e14be4555922e8e5e1765
NateScarlet <NateScarlet@Gmail.com>
2020-09-21 14:13:57 +0800

    feat(api): add Mutation.updateCollectionMetadata

---
+++ pkg/api/resolvers/updateCollectionMetadata.resolvers.go
@@ -0,0 +1,42 @@
+package resolvers
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+
+	"github.com/WuLiFang/csheet/v6/pkg/api/generated/model"
+	"github.com/WuLiFang/csheet/v6/pkg/model/collection"
+)
+
+func (r *mutationResolver) UpdateCollectionMetadata(ctx context.Context, input model.UpdateCollectionMetadataInput) (*model.UpdateCollectionMetadataPayload, error) {
+	var ret = new(model.UpdateCollectionMetadataPayload)
+	var err error
+	ret.ClientMutationID = input.ClientMutationID
+	var m = make(map[string]collection.Collection)
+
+	for _, i := range input.Data {
+		v, ok := m[i.ID]
+		if !ok {
+			v, err = collection.FindByID(i.ID)
+			if err != nil {
+				return ret, err
+			}
+			m[i.ID] = v
+		}
+		if i.Value == "" {
+			delete(v.Metadata, i.Key)
+		} else {
+			v.Metadata[i.Key] = i.Value
+		}
+	}
+
+	ret.Updated = make([]collection.Collection, 0, len(m))
+	for _, i := range m {
+		i.Save()
+		ret.Updated = append(ret.Updated, i)
+	}
+
+	return ret, err
+}
