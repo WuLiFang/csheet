b2f11f1c2e499e8784e17d13d5bc0cb8dde0f5e7
NateScarlet <NateScarlet@Gmail.com>
2020-11-02 18:12:36 +0800

    feat: cgteamwork loading indicator

--- src/components/cgteamwork/CGTeamworkNoteList.vue
+++ src/components/cgteamwork/CGTeamworkNoteList.vue
@@ -4,24 +4,29 @@
       span 备注
       button.form-button(
         class="inline-flex flex-center"
-        class="h-6 py-0 px-2 mx-1"
+        class="p-0 px-2 h-6 mx-1"
         type="button"
-        @click="refetch()"
-      )
+        @click="refetch().then(() => $root.$emit('app-message', '刷新完成'))"
+      ) 
         FaIcon(name="sync" class="mx-1")
         span 刷新
-    ol(
-      class="ml-2 space-y-1"
-    )
-      template(v-for="i in values")
-        CGTeamworkNoteListItem(:value="i" :hide-pipeline="pipelines.length === 1")
-    template(v-if="values.length === 0")
+    template(v-if="loadingCount > 0 ")
+      FaIcon(name="spinner" spin class="w-full inline-block h-8 text-gray-500")
+    template(v-else-if="values.length === 0")
       p.text-gray-600.text-center 无备注
+    template(v-else)
+      ol(
+        class="ml-2 space-y-1"
+      )
+        template(v-for="i in values")
+          CGTeamworkNoteListItem(:value="i" :hide-pipeline="pipelines.length === 1")
 </template>
 
 <script lang="ts">
 import { Component, Vue, Prop } from 'vue-property-decorator';
-import CGTeamworkNoteListItem, {CGTeamworkNoteListItemValue} from './CGTeamworkNoteListItem.vue';
+import CGTeamworkNoteListItem, {
+  CGTeamworkNoteListItemValue,
+} from './CGTeamworkNoteListItem.vue';
 import {
   collectionNodeVariables,
   collectionNode,
@@ -34,7 +39,7 @@ import { filePathFormat } from '@/const';
 import { collection as Collection } from '@/graphql/types/collection';
 import cast from 'cast-unknown';
 import { orderBy } from 'lodash';
-import "vue-awesome/icons/sync"
+import 'vue-awesome/icons/sync';
 
 @Component<CGTeamworkNoteList>({
   components: {
@@ -53,27 +58,25 @@ import "vue-awesome/icons/sync"
     values: {
       query: require('@/graphql/queries/collectionCGTeamworkNotes.gql'),
       variables(): collectionCGTeamworkNotesVariables {
-        return {
-          id: this.id,
-          pipeline: this.formData.pipeline
-            ? [this.formData.pipeline]
-            : undefined,
-        };
+        return { id: this.id };
       },
       update(v: collectionCGTeamworkNotes): CGTeamworkNoteListItemValue[] {
         return orderBy(
-          ( v.node?.__typename === 'Collection'
-          ? v.node.cgteamworkNotes ?? []
-          : []).flatMap(i => i.notes.map(j => ({
-            ...j,
-            pipeline: i.pipeline,
-            created: cast.date(j.created),
-          }))),
+          (v.node?.__typename === 'Collection'
+            ? v.node.cgteamworkNotes ?? []
+            : []
+          ).flatMap(i =>
+            i.notes.map(j => ({
+              ...j,
+              pipeline: i.pipeline,
+              created: cast.date(j.created),
+            }))
+          ),
           i => i.created,
-          "desc"
+          'desc'
         );
       },
-      fetchPolicy: "cache-and-network",
+      fetchPolicy: 'cache-and-network',
     },
   },
 })
@@ -82,11 +85,9 @@ export default class CGTeamworkNoteList extends Vue {
   id!: string; // Colleciton id.
 
   node?: Collection;
-  values?: CGTeamworkNoteListItemValue[];
+  values: CGTeamworkNoteListItemValue[] = [];
 
-  formData = {
-    pipeline: '',
-  };
+  loadingCount = 0;
 
   get pipelines(): string[] {
     return cast
@@ -99,7 +100,7 @@ export default class CGTeamworkNoteList extends Vue {
   }
 
   async refetch(): Promise<void> {
-    await this.$apollo.queries.values.refetch()
+    await this.$apollo.queries.values.refetch();
   }
 }
 </script>
