0075c34561f0feb7d46182f7b65486fc32d20f9e
NateScarlet <NateScarlet@Gmail.com>
2020-11-05 16:52:39 +0800

    fix(db): migration transaction too large

--- pkg/db/migrations/v2.go
+++ pkg/db/migrations/v2.go
@@ -104,157 +104,163 @@ func v2MarshalValue(v interface{}) (ret []byte, err error) {
 }
 
 func useBase64ID(db *badger.DB) error {
-	return db.Update(func(txn *badger.Txn) (err error) {
+	return db.View(func(txn *badger.Txn) (err error) {
 		cur := txn.NewIterator(badger.IteratorOptions{})
 		defer cur.Close()
 		for cur.Seek([]byte{}); cur.Valid(); cur.Next() {
-			item := cur.Item()
-			key := item.KeyCopy(nil)
-			if key[0] != 0x00 {
-				return fmt.Errorf("unknown database key: %q", key)
-			}
-			switch key[1] {
-
-			// collection
-			case 0x03:
-				var oldID = string(key[2 : len(key)-1])
-				var newID []byte
-				newID, err = base64ID(oldID)
-				if err != nil {
-					return
+			err = db.Update(func(txn *badger.Txn) (err error) {
+				item := cur.Item()
+				key := item.KeyCopy(nil)
+				if key[0] != 0x00 {
+					return fmt.Errorf("unknown database key: %q", key)
 				}
-				item.Value(func(val []byte) (err error) {
-					var doc = new(v2Collection)
-					err = v2UnmarshalValue(val, doc)
+				switch key[1] {
+
+				// collection
+				case 0x03:
+					var oldID = string(key[2 : len(key)-1])
+					var newID []byte
+					newID, err = base64ID(oldID)
 					if err != nil {
 						return
 					}
-					for i := range doc.PresentationIDs {
-						var v = doc.PresentationIDs[i]
-						var idData []byte
-						idData, err = base64.RawURLEncoding.DecodeString(v)
+					item.Value(func(val []byte) (err error) {
+						var doc = new(v2Collection)
+						err = v2UnmarshalValue(val, doc)
 						if err != nil {
 							return
 						}
-						var newID []byte
-						newID, err = base64ID(string(idData[2 : len(idData)-1]))
+						for i := range doc.PresentationIDs {
+							var v = doc.PresentationIDs[i]
+							var idData []byte
+							idData, err = base64.RawURLEncoding.DecodeString(v)
+							if err != nil {
+								return
+							}
+							var newID []byte
+							newID, err = base64ID(string(idData[2 : len(idData)-1]))
+							if err != nil {
+								return
+							}
+							doc.PresentationIDs[i] = base64.RawURLEncoding.EncodeToString(
+								append(append(append([]byte{}, idData[:2]...), newID...), 0x09),
+							)
+							if err != nil {
+								return
+							}
+						}
+						var newValue []byte
+						newValue, err = v2MarshalValue(doc)
 						if err != nil {
 							return
 						}
-						doc.PresentationIDs[i] = base64.RawURLEncoding.EncodeToString(
-							append(append(append([]byte{}, idData[:2]...), newID...), 0x09),
+						return txn.Set(
+							append(append(append(make([]byte, 0, 2+len(newID)), key[:2]...), newID...), 0x09),
+							newValue,
 						)
+					})
+					err = txn.Delete(key)
+					if err != nil {
+						return err
+					}
+
+				// primary key
+				case 0x05:
+					var oldID = string(key[2 : len(key)-1])
+					var newID []byte
+					newID, err = base64ID(oldID)
+					if err != nil {
+						return
+					}
+					err = renameEntry(txn,
+						key,
+						append(append(append(make([]byte, 0, 2+len(newID)), key[:2]...), newID...), 0x09),
+					)
+					if err != nil {
+						return err
+					}
+
+				// primary id
+				case 0x09:
+					var oldB64ID = string(key[2 : len(key)-1])
+					var oldIDData []byte
+					oldIDData, err = base64.RawURLEncoding.DecodeString(oldB64ID)
+					if err != nil {
+						return
+					}
+					var oldID = string(oldIDData[2 : len(oldIDData)-1])
+					var newID []byte
+					newID, err = base64ID(oldID)
+					if err != nil {
+						return
+					}
+					err = renameEntry(txn,
+						key,
+						append(append(append(make([]byte, 0, 2+len(newID)), key[:2]...), newID...), 0x09),
+					)
+					if err != nil {
+						return err
+					}
+
+				// secondary key
+				case 0x04:
+					fallthrough
+				case 0x06:
+					err = item.Value(func(val []byte) (err error) {
+						oldID := unmarshalBSONString(val)
+						newID, err := base64ID(oldID)
 						if err != nil {
 							return
 						}
+						err = txn.Set(key, newID)
+						if err != nil {
+							return
+						}
+						return
+					})
+					if err != nil {
+						return err
 					}
-					var newValue []byte
-					newValue, err = v2MarshalValue(doc)
+
+				// secondary id
+				case 0x08:
+					var pathEnd = bytes.IndexByte(key, 0x09)
+					var path = key[2:pathEnd]
+					var oldB64ID = string(key[pathEnd+1 : len(key)-1])
+					var oldIDData []byte
+					oldIDData, err = base64.RawURLEncoding.DecodeString(oldB64ID)
 					if err != nil {
 						return
 					}
-					return txn.Set(
-						append(append(append(make([]byte, 0, 2+len(newID)), key[:2]...), newID...), 0x09),
-						newValue,
+					if oldIDData[0] != 0x00 && oldIDData[0] != 0x05 {
+						return fmt.Errorf("invalid presentation id: %x", key)
+					}
+					var oldID = string(oldIDData[2 : len(oldIDData)-1])
+					var newID []byte
+					newID, err = base64ID(oldID)
+					if err != nil {
+						return
+					}
+					err = renameEntry(
+						txn,
+						key,
+						append(append(append(append(append(make([]byte, 0, 16), key[:2]...), path...), 0x09), newID...), 0x09),
 					)
-				})
-				err = txn.Delete(key)
-				if err != nil {
-					return err
-				}
-
-			// primary key
-			case 0x05:
-				var oldID = string(key[2 : len(key)-1])
-				var newID []byte
-				newID, err = base64ID(oldID)
-				if err != nil {
-					return
-				}
-				err = renameEntry(txn,
-					key,
-					append(append(append(make([]byte, 0, 2+len(newID)), key[:2]...), newID...), 0x09),
-				)
-				if err != nil {
-					return err
-				}
-
-			// primary id
-			case 0x09:
-				var oldB64ID = string(key[2 : len(key)-1])
-				var oldIDData []byte
-				oldIDData, err = base64.RawURLEncoding.DecodeString(oldB64ID)
-				if err != nil {
-					return
-				}
-				var oldID = string(oldIDData[2 : len(oldIDData)-1])
-				var newID []byte
-				newID, err = base64ID(oldID)
-				if err != nil {
-					return
-				}
-				err = renameEntry(txn,
-					key,
-					append(append(append(make([]byte, 0, 2+len(newID)), key[:2]...), newID...), 0x09),
-				)
-				if err != nil {
-					return err
-				}
-
-			// secondary key
-			case 0x04:
-				fallthrough
-			case 0x06:
-				err = item.Value(func(val []byte) (err error) {
-					oldID := unmarshalBSONString(val)
-					newID, err := base64ID(oldID)
 					if err != nil {
 						return
 					}
-					err = txn.Set(key, newID)
-					if err != nil {
-						return
-					}
-					return
-				})
-				if err != nil {
-					return err
-				}
 
-			// secondary id
-			case 0x08:
-				var pathEnd = bytes.IndexByte(key, 0x09)
-				var path = key[2:pathEnd]
-				var oldB64ID = string(key[pathEnd+1 : len(key)-1])
-				var oldIDData []byte
-				oldIDData, err = base64.RawURLEncoding.DecodeString(oldB64ID)
-				if err != nil {
-					return
+				case 0x00:
+				case 0x01:
+				case 0x07:
+				case 0x0a:
+				default:
+					return fmt.Errorf("unknown database key: %q", key)
 				}
-				if oldIDData[0] != 0x00 && oldIDData[0] != 0x05 {
-					return fmt.Errorf("invalid presentation id: %x", key)
-				}
-				var oldID = string(oldIDData[2 : len(oldIDData)-1])
-				var newID []byte
-				newID, err = base64ID(oldID)
-				if err != nil {
-					return
-				}
-				err = renameEntry(
-					txn,
-					key,
-					append(append(append(append(append(make([]byte, 0, 16), key[:2]...), path...), 0x09), newID...), 0x09),
-				)
-				if err != nil {
-					return
-				}
-
-			case 0x00:
-			case 0x01:
-			case 0x07:
-			case 0x0a:
-			default:
-				return fmt.Errorf("unknown database key: %q", key)
+				return
+			})
+			if err != nil {
+				return
 			}
 		}
 		return nil
