a4eaf8efc420a0bcba03f1bc9998e911d93bd4ab
NateScarlet <NateScarlet@Gmail.com>
2020-12-04 15:59:03 +0800

    chore: clean unused file after codegen

--- scripts/codegen-graphql.ts
+++ scripts/codegen-graphql.ts
@@ -1,25 +1,63 @@
 #!/usr/bin/env ts-node-script
 
 import { spawnSync } from 'child_process';
-import { utimesSync } from 'fs';
+import { promises as fs } from 'fs';
+import * as path from 'path';
+const { readFile, utimes, readdir, unlink, stat } = fs;
 
-spawnSync(
-  'apollo',
-  [
-    'client:codegen',
-    '--passthroughCustomScalars ',
-    '--customScalarsPrefix',
-    "import('./scalars').",
-    '--target',
-    'typescript',
-    '--globalTypesFile',
-    'src/graphql/types/global.ts ',
-    '--outputFlat',
-    'src/graphql/types',
-  ],
-  {
-    shell: true,
-    stdio: 'inherit',
+async function isGeneratedFile(path: string): Promise<boolean> {
+  if (!path.endsWith('.ts')) {
+    return false;
   }
-);
-utimesSync('src/graphql/types', new Date(), new Date());
+
+  const data = await readFile(path);
+  return data.includes('\n// @generated\n');
+}
+
+(async (): Promise<void> => {
+  const startTime = new Date();
+  const dir = 'src/graphql/types';
+
+  spawnSync(
+    'apollo',
+    [
+      'client:codegen',
+      '--passthroughCustomScalars ',
+      '--customScalarsPrefix',
+      "import('./scalars').",
+      '--target',
+      'typescript',
+      '--globalTypesFile',
+      path.join(dir, 'global.ts'),
+      '--outputFlat',
+      dir,
+    ],
+    {
+      shell: true,
+      stdio: 'inherit',
+    }
+  );
+
+  // cleanup unused files
+  for (const i of await readdir(dir)) {
+    if (!i.endsWith('.ts')) {
+      continue;
+    }
+
+    const p = path.join(dir, i);
+    const { mtime } = await stat(p);
+    if (mtime > startTime) {
+      continue;
+    }
+    if (!(await isGeneratedFile(p))) {
+      continue;
+    }
+    await unlink(p);
+    console.log({ msg: 'remove', path: p });
+  }
+
+  await utimes(dir, new Date(), new Date());
+})().catch(err => {
+  console.error(err);
+  process.exit(1);
+});
