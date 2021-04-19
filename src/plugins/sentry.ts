c6588c132324549dcb68ec647ac3a15941045c1b
NateScarlet <NateScarlet@Gmail.com>
2021-04-19 12:21:43 +0800

    fix: disable sentry requestAnimationFrame wrap for chrome74.0.3729
    https://github.com/getsentry/sentry-javascript/issues/3388

--- src/plugins/sentry.ts
+++ src/plugins/sentry.ts
@@ -8,10 +8,24 @@ import Vue from 'vue';
   if (!config?.sentryDSN) {
     return;
   }
+  // https://github.com/getsentry/sentry-javascript/issues/3388
+  const isAffectByIssue3388 = navigator.userAgent.includes('Chrome/74.0.3729');
   sentry.init({
     environment: process.env.NODE_ENV,
     dsn: config.sentryDSN,
     release: RELEASE,
-    integrations: [new SentryVue({ Vue, attachProps: true })],
+    defaultIntegrations: false,
+    integrations: [
+      new sentry.Integrations.InboundFilters(),
+      new sentry.Integrations.FunctionToString(),
+      new sentry.Integrations.TryCatch({
+        requestAnimationFrame: !isAffectByIssue3388,
+      }),
+      new sentry.Integrations.Breadcrumbs(),
+      new sentry.Integrations.GlobalHandlers(),
+      new sentry.Integrations.LinkedErrors(),
+      new sentry.Integrations.UserAgent(),
+      new SentryVue({ Vue, attachProps: true }),
+    ],
   });
 })();
